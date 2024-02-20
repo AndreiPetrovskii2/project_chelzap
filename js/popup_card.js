872cf6d04726ea061272fe4b3b50a71c1b87b4a2
andrei petrovsky <andrei_2001pet@mail.ru>
2024-02-21 03:00:38 +0500

    popup full + buttons

--- js/popup_card.js
+++ js/popup_card.js
@@ -1,12 +1,29 @@
 let card1 = document.getElementById('card1');
 let button_popup = document.getElementById('popup_text');
 let card_text = document.getElementById('card_text');
-let rewiws = document.getElementById('body');
+let rewiws = document.getElementById('rewiws');
+let img1 = document.getElementById('img1');
+let img2 = document.getElementById('img2');
+let img3 = document.getElementById('img3');
+let img4 = document.getElementById('img4');
+let button1 = document.getElementById('left');
+let button2 = document.getElementById('right');
+let out = document.getElementById('out');
 button_popup.addEventListener('click',function(){
     card1.style='height: 414px;';
     button_popup.style='display:none';
     card_text.style='overflow: visible;-webkit-line-clamp: unset;line-height: 22.4px;';
-})
-/*body.addEventListener('click',function(){
-    card1.style=' height: 264px;';
-});*/
+    rewiws.style='background-color: rgba(0, 0, 0, 0.5);';
+    button1.style='opacity:1%';
+    button2.style='opacity:1%';
+    out.style='display:flex; padding-top: 169px;justify-content: center;'
+});
+out.addEventListener('click',function(){
+    card1.style='height: 264px;';
+    button_popup.style='display:initial';
+    card_text.style='overflow: hiden;-webkit-line-clamp: 6;';
+    rewiws.style='background-color: #EEF3F6;';
+    button1.style='opacity:100%';
+    button2.style='opacity:100%';
+    out.style='display:none;'
+});
--- js/slider.js
+++ js/slider.js
@@ -3,6 +3,8 @@ let left = document.getElementById('left');
 let right = document.getElementById('right');
 let offset = 0;
 right.addEventListener('click' ,function(){
+    left.style= 'background-color: #EEF3F6;';
+    right.style= 'background-color: #FFFFFF;border:none;';
     offset += 410;
     if(offset > 410){
         offset = 410;
@@ -10,9 +12,13 @@ right.addEventListener('click' ,function(){
     slider.style.left = -offset + 'px';
 });
 left.addEventListener('click' ,function(){
+    right.style= 'background-color: #EEF3F6;';
+    left.style= 'background-color: #FFFFFF;border:none;';
     offset -= 410;
     if(offset < 0){
         offset = 0;
     }
     slider.style.left = -offset + 'px';
 });
+
+

bae614b4ae6e3683d782a3fdb4c1e8aaa3f7cac2
andrei petrovsky <andrei_2001pet@mail.ru>
2024-02-23 03:49:36 +0500

    99% adaptation + add menu

---
+++ js/drop_window.js
@@ -0,0 +1,16 @@
+let second_header = document.getElementById('second_header');
+let close_btn = document.getElementById('close_btn');
+let first_header = document.getElementById('first_header');
+let drop_down = document.getElementById('drop_down');
+let main = document.getElementById('main');
+
+drop_down.addEventListener('click', function (){
+    second_header.style= 'display:flex';
+    first_header.style ='display:none';
+    main.style = 'filter: blur(1px);'
+})
+close_btn.addEventListener('click', function (){
+    second_header.style= 'display:none';
+    first_header.style ='display:flex';
+    main.style = 'filter: blur(0px);'
+})

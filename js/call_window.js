c53c6b627c9d63a673465f2256843df7867fbed9
andrei petrovsky <andrei_2001pet@mail.ru>
2024-02-23 14:20:07 +0500

    90%

---
+++ js/call_window.js
@@ -0,0 +1,6 @@
+/*let call = document.getElementById('ring');
+let input_window = document.getElementById('input');
+
+input_window.addEventListener('click',function (){
+    call.style = 'display:flex';
+})*/

ff966bd6e0db572f80c723e071f99e16e04e1df6
andrei petrovsky <andrei_2001pet@mail.ru>
2024-02-26 03:06:00 +0500

    fix input mask + call_window

--- js/call_window.js
+++ js/call_window.js
@@ -1,12 +1,32 @@
 let call = document.getElementById('ring');
-let input_window = document.getElementById('input');
+let input_window = document.getElementById('input4');
 let close = document.getElementById('img_close');
 var main2 = document.getElementById('main');
+let shadow = document.getElementById('shadow');
+let shadow2 = document.getElementById('shadow2');
+let shadow_header = document.getElementById('shadow_header');
+let shadow_img = document.getElementById('shadow_img');
+let shadow_img2 = document.getElementById('shadow_img2');
+let shadow_img3 = document.getElementById('shadow_img3');
+let shadow_dignities = document.getElementById('shadow_dignities')
 
 input_window.addEventListener('click',function (){
-    call.style = 'display:flex;filter: blur(0px);';
+    call.style = 'display:flex;';
+    shadow.style = 'filter: blur(2px);';
+    shadow2.style = 'filter: blur(2px);';
+    shadow_header.style = 'filter: blur(2px);';
+    shadow_img.style = 'filter: blur(2px);';
+    shadow_img2.style = 'filter: blur(2px);';
+    shadow_img3.style = 'filter: blur(2px);';
+    shadow_dignities.style = 'filter: blur(2px);';
 })
 close.addEventListener('click',function (){
     call.style = 'display:none';
-    main2.style = 'filter: blur(0px);'
+    shadow.style = 'filter: blur(0px);';
+    shadow2.style = 'filter: blur(0px);';
+    shadow_header.style = 'filter: blur(0px);';
+    shadow_img.style = 'filter: blur(0px);';
+    shadow_img2.style = 'filter: blur(0px);;';
+    shadow_img3.style = 'filter: blur(0px);';
+    shadow_dignities.style = 'filter: blur(0px);';
 })
--- js/input_mask.js
+++ js/input_mask.js
@@ -1,13 +1,16 @@
-let input_header = document.getElementById('input');
-let input_second = document.getElementById('input_second2')
+let input_header = document.getElementById('input4');
+const phoneMask4 = new IMask(input_header, {
+    mask: "+{7}(000)000-00-00",
+  });
+let input_second2 = document.getElementById('input_second2')
 let input_header2 = document.getElementById('input2');
-
-const phoneMask = new IMask(input_header, {
-  mask: "+{7}(000)000-00-00",
-});
+let input_second = document.getElementById('input_second')
 const phoneMask2 = new IMask(input_second2, {
   mask: "+{7}(000)000-00-00",
 });
 const phoneMask3 = new IMask(input_header2, {
+  mask: "+{7}(000)000-00-00",
+});
+const phoneMask = new IMask(input_second, {
   mask: "+{7}(000)000-00-00",
 });

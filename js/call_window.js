c2176d8f789953b9b8da7da502e77b5f111cdd4d
andrei petrovsky <andrei_2001pet@mail.ru>
2024-02-25 15:57:44 +0500

    drop window add

--- js/call_window.js
+++ js/call_window.js
@@ -5,7 +5,6 @@ var main2 = document.getElementById('main');
 
 input_window.addEventListener('click',function (){
     call.style = 'display:flex;filter: blur(0px);';
-    main2.style = 'filter: blur(1px);'
 })
 close.addEventListener('click',function (){
     call.style = 'display:none';

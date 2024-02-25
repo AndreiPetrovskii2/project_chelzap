839eafd0dfa4f414b3bb5ad2fca20def0bbe89e2
andrei petrovsky <andrei_2001pet@mail.ru>
2024-02-26 02:31:13 +0500

    fix input mask

--- js/input_mask.js
+++ js/input_mask.js
@@ -1,9 +1,13 @@
 let input_header = document.getElementById('input');
-let input_second = document.getElementById('input_second')
+let input_second = document.getElementById('input_second2')
+let input_header2 = document.getElementById('input2');
 
 const phoneMask = new IMask(input_header, {
   mask: "+{7}(000)000-00-00",
 });
-const phoneMask2 = new IMask(input_second, {
+const phoneMask2 = new IMask(input_second2, {
+  mask: "+{7}(000)000-00-00",
+});
+const phoneMask3 = new IMask(input_header2, {
   mask: "+{7}(000)000-00-00",
 });

4a4db17140140be03533deb4909206b258b632e4
andrei petrovsky <andrei_2001pet@mail.ru>
2024-02-25 15:57:12 +0500

    drop window

--- js/call_window.js
+++ js/call_window.js
@@ -1,6 +1,13 @@
-/*let call = document.getElementById('ring');
+let call = document.getElementById('ring');
 let input_window = document.getElementById('input');
+let close = document.getElementById('img_close');
+var main2 = document.getElementById('main');
 
 input_window.addEventListener('click',function (){
-    call.style = 'display:flex';
-})*/
+    call.style = 'display:flex;filter: blur(0px);';
+    main2.style = 'filter: blur(1px);'
+})
+close.addEventListener('click',function (){
+    call.style = 'display:none';
+    main2.style = 'filter: blur(0px);'
+})
